eb585f3517d5cf500e809cb254e127fa107d656f
skyler dennis <skylerdenniss11@gmail.com>
2024-09-19 19:46:38 -0400

    Finnished creating interpulated story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StoryPrograme();
         }
+
+
+
+        public static void StoryPrograme()
+        {
+
+            Console.WriteLine("What is your name?");
+            string username = Console.ReadLine();
+            Console.WriteLine($"Hello {username}! What is your favorite color?");
+            string favcolor = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string favband = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favanimal = Console.ReadLine();
+
+            Console.WriteLine($"One day {username} was driving into town in there {favcolor} car, As {username} was driving they could her the band {favband} playing and to there surprise a {favanimal} dancing to it!");
+ 
+        }
+
+
+
     }
 }
+
+
+
+
+
+
+
+    
+    
+  
+
+
+   
+   
+   
+    
+
